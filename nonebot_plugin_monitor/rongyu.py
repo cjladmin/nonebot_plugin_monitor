2804416b888ba991627a17e12fe4be095d38b3ee
Torres-圣君 <53791401+cjladmin@users.noreply.github.com>
2022-03-29 18:27:54 +0800

    适配onebot.V11

---
+++ nonebot_plugin_monitor/rongyu.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+# @Time : 2022/1/19 12:05
+# @Author : Torres-圣君
+# @File : rongyu.py
+# @Sofaware : PyCharm
+
+# 机器人QQ号
+bot_qq = 3404406097
+# 开发者QQ号
+super_qq = 2653644677
+
+
+def monitor_rongyu(honor_type, user_id):
+    rely = ""
+    # 龙王
+    if honor_type == "talkative":
+        if user_id == bot_qq:
+            rely = f"你们又不行了，本喵喜提龙王~"
+        elif user_id == super_qq:
+            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获龙王标识~"
+        else:
+            rely = f"恭喜[CQ:at,qq={user_id}]荣获龙王标识~"
+    # 群聊之火
+    elif honor_type == "performer":
+        if user_id == bot_qq:
+            pass
+        elif user_id == super_qq:
+            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获群聊之火标识~"
+        else:
+            rely = f"恭喜[CQ:at,qq={user_id}]荣获群聊之火标识~"
+    # 快乐源泉
+    elif honor_type == "emotion":
+        if user_id == bot_qq:
+            pass
+        elif user_id == super_qq:
+            rely = f"[CQ:at,qq={user_id}]恭喜主人荣获快乐源泉标识~"
+        else:
+            rely = f"恭喜[CQ:at,qq={user_id}]荣获快乐源泉标识~"
+    return rely
