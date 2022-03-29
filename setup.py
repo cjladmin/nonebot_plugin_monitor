839ba44b7ef5ba70ac53198a7644f03798349521
Torres-圣君 <53791401+cjladmin@users.noreply.github.com>
2022-03-29 18:29:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="基于NoneBot2实现，检测QQ群状态，例如：群成员人数变动、文件上传、红包运气王、管理员变动等",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cjladmin/nonebot_plugin_monitoring",
+    url="https://github.com/cjladmin/nonebot_plugin_monitor",
     license='MIT',
     install_requires=[
         "requests>=2.26.0",
