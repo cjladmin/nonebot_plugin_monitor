223bc83ef65c3b3f2e17fe79632356a666b664c6
Torres-圣君 <53791401+cjladmin@users.noreply.github.com>
2022-03-29 18:28:26 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nonebot_plugin_monitor",
+    version="0.0.1",
+    author="Torres-圣君",
+    author_email="2653644677@qq.com",
+    description="基于NoneBot2实现，检测QQ群状态，例如：群成员人数变动、文件上传、红包运气王、管理员变动等",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cjladmin/nonebot_plugin_monitoring",
+    license='MIT',
+    install_requires=[
+        "requests>=2.26.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3'
+)
